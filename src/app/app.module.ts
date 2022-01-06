01003305c0e85f5b877caa04b0d2c13c847d2618
DÁNIEL ÁDÁM ZOLTÁN <daniel.adam995@gmail.com>
2022-01-06 19:33:21 +0100

    Version 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+// import { HttpClientModule } from '@angular/common/http';
+// import { FormsModule } from '@angular/forms';  
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HttpClientModule,
+    // FormsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
