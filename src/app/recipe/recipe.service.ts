a81b6e293f9f706ee2eeed15213fb09782f2e136
DÁNIEL ÁDÁM ZOLTÁN <daniel.adam995@gmail.com>
2022-01-31 23:19:22 +0100

    version 0.0.16.2

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -10,7 +10,19 @@ export class RecipeService {
 
   constructor(private http: HttpClient) { }
 
-//   public getRecipeCard(): Observable<RecipeCard[]>{
-//     return this.http.get<RecipeCard[]>(`${this.apiServerUrl}/home/all`);
-// }
+  public getRecipeCard(): Observable<RecipeCard[]>{
+    return this.http.get<RecipeCard[]>(`${this.apiServerUrl}/home/all`);
+
+    public addUser(user: User): Observable<User>{
+      return this.http.post<User>(`${this.apiServerUrl}/user/add`, user);
+  }
+
+  // public updateUser(user: User): Observable<User>{
+  //     return this.http.put<User>(`${this.apiServerUrl}/user/update`, user);
+  // }
+
+  // public deleteUser(userId: number): Observable<void>{
+  //     return this.http.delete<void>(`${this.apiServerUrl}/user/delete/${userId}`);
+  // }
+}
 }
